c2df289e2bd20d8660801f8b3b3159f149a12e35
Uchiha Madara <157013154+Uchihamadara3548@users.noreply.github.com>
2024-06-20 14:44:16 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("f").onsubmit = function() {
-  // Perform any login validation if needed
-  // Redirect to second.html
-  window.location.href = "final.html";
-  return false; // Prevent the form from submitting traditionally
-};
